f52ee408f90fe307469bc2798670fedef6d0d365
blakeadams2355-spec <blakeadams2355@gmail.com>
2026-01-03 17:00:24 +0300

    Hide config file

---
+++ config.py
@@ -0,0 +1,21 @@
+# Конфигурация бота
+
+# Токен бота от @BotFather
+BOT_TOKEN = "8059522147:AAF4xjxCMxcAyF8NTev2JNC1RJIumkPa1Es"
+
+# Твой Telegram ID (только ты сможешь использовать бота)
+ADMIN_ID = 8181234841  # Замени на свой ID
+
+# Настройки по умолчанию
+DEFAULT_SETTINGS = {
+    "daily_reminder": False,
+    "daily_reminder_time": "21:00",
+    "weekly_reminder": False,
+    "weekly_reminder_day": "monday",
+    "weekly_reminder_time": "10:00",
+    "default_currency": "RUB",  # Валюта по умолчанию
+    "usd_rub_rate": 100.0,  # Курс доллара к рублю
+}
+
+# Путь к базе данных
+DATABASE_PATH = "sales_bot.db"
